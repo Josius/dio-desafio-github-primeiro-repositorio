f07bec3652fd8318953e4ea9707316fe7cabe701
JosiusW <josius.amaro@gmail.com>
2021-12-07 14:39:14 -0300

    primeiros passo do dotnet, criando uma estrutura de pilhas com dotnet

---
+++ iniciandoComDotNet/DigitalInnovationOne/Program.cs
@@ -0,0 +1,24 @@
+﻿
+using System;
+
+namespace DigitalInnovationOne{
+    class Program{
+        static void Main(string[] args){
+            int numeroDeVezes = 5;
+
+            for(int i=0; i < numeroDeVezes; i++){
+                Console.WriteLine($"Bem-vindo ao curso de .NET {i}");
+            }
+        }
+    }
+}
+
+// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+/*
+int numeroDeVezes = 5;
+
+for(int i=0; i < numeroDeVezes; i++){
+    Console.WriteLine($"Bem-vindo ao curso de .NET {i}");
+}
+*/
